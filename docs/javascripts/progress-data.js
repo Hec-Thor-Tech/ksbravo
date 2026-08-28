8ddc8873e8fda2add4bc2b40902029da673292e2
Hec-Thor-Tech <bravo.policiaciudad@gmail.com>
2026-08-27 23:37:30 -0300

    Actualizacion desde el Gestor Web

--- docs/javascripts/progress-data.js
+++ docs/javascripts/progress-data.js
@@ -13,7 +13,7 @@
    ===================================================================== */
 
 window.KS_PROGRESS = {
-  updated: "2026-08-25",
+  updated: "2026-08-27",
   packs: [
     {
       name: "3D Memes Bonus Pack 01",
@@ -22,7 +22,7 @@ window.KS_PROGRESS = {
       total: 30,
       // img = la referencia 2D del personaje, en docs/img/
       models: [
-        { name: "Pellow",    steps: 15, img: "ref-pellow.webp", imgv: "1787199448" },
+        { name: "Pellow",    steps: 18, img: "ref-pellow.webp", imgv: "1787199448" },
         { name: "Umnidorid", steps: 30, img: "ref-umnidorid.webp", imgv: "1787253870" },
         { name: "Tim",       steps:  2, img: "ref-tim.webp", imgv: "1787199448" },
         { name: "Megmen",    steps: 10, img: "ref-megmen.webp", imgv: "1787199449" },
