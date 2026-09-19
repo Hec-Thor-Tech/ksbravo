bfd9d4246f14b416671b623e7dfb9b0b14bf2f41
Hec-Thor-Tech <bravo.policiaciudad@gmail.com>
2026-09-18 22:24:52 -0300

    Actualizacion desde el Gestor Web

--- docs/javascripts/progress-data.js
+++ docs/javascripts/progress-data.js
@@ -22,12 +22,12 @@ window.KS_PROGRESS = {
       total: 30,
       // img = la referencia 2D del personaje, en docs/img/
       models: [
-        { name: "Pellow",    steps: 30, img: "ref-pellow.webp", imgv: "1789774196", group: "pellow" },
+        { name: "Pellow",    steps: 30, img: "ref-pellow.webp", imgv: "1789774196", video: "y4T2OjN7Lj8", group: "pellow" },
         { name: "Umnidorid", steps: 30, img: "ref-umnidorid.webp", imgv: "1787253870", video: "a6VqXX8Njzk" },
         { name: "Tim",       steps: 30, img: "ref-tim.webp", imgv: "1787199448", video: "COssIbk5HGw" },
         { name: "Megmen",    steps: 27, img: "ref-megmen.webp", imgv: "1787199449" },
         { name: "Noobstar",  steps: 29, img: "ref-noobstar.webp", imgv: "1787199450" },
-        { name: "Nyoan Cot", steps: 30, img: "ref-nyoancot.webp", imgv: "1787199451" }
+        { name: "Nyoan Cot", steps: 30, img: "ref-nyoancot.webp", imgv: "1787199451", video: "y4T2OjN7Lj8" }
       ]
     },
     {
@@ -41,7 +41,7 @@ window.KS_PROGRESS = {
         { name: "Whyti",            steps: 20, img: "ref-whyti.webp", imgv: "1788108347" },
         { name: "Criim Teh Ribbit", steps:  6, img: "ref-criimtehribbit.webp", imgv: "1788108348" },
         { name: "Ezzyne",           steps:  5, img: "ref-ezzyne.webp", imgv: "1788108349" },
-        { name: "Pellow",           steps: 30, img: "ref-pellow.webp", imgv: "1789774196", key: "pellowv2", group: "pellow" },
+        { name: "Pellow",           steps: 30, img: "ref-pellow.webp", imgv: "1789774196", video: "y4T2OjN7Lj8", key: "pellowv2", group: "pellow" },
         { name: "Desploy",          steps: 30, img: "ref-desploy.webp", imgv: "1788108351", video: "hvQj4yjAmQs" },
         { name: "Cerrut",           steps:  6, img: "ref-cerrut.webp", imgv: "1789772221" }
       ]
